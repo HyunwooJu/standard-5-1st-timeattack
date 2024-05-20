ea904cffb1e12a5d53237ad1c381612bfcca1795
HyunwooJu <soc05104@gmail.com>
2024-05-20 15:48:39 +0900

    계산기 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import React, { useState } from "react";
+
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState(0);
+
+  const handleAddition = () => {
+    setResult((prevResult) => prevResult + parseFloat(inputValue));
+    setInputValue("");
+  };
+
+  const handleSubtraction = () => {
+    setResult((prevResult) => prevResult - parseFloat(inputValue));
+    setInputValue("");
+  };
+
+  const handleReset = () => {
+    setResult(0);
+    setInputValue("");
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {""}
+        만큼을 {""}
+        <button onClick={handleAddition}>더할게요</button>{" "}
+        <button onClick={handleSubtraction}>뺄게요</button>
+        <button onClick={handleReset}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result}</p>
       </div>
     </div>
   );
